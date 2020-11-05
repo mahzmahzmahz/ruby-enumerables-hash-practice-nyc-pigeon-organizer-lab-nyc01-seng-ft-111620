3f86ffe435925b6c8d5822d90e32eaedd4d1db4a
Alex Marz <alexhmarz@gmail.com>
2020-11-05 18:26:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
-  data.each_with_object({}) do |name, description| 
+  data.each_with_object({}) do |(name, description), final_data|
+end
   
   # write your code here!
   binding.pry
