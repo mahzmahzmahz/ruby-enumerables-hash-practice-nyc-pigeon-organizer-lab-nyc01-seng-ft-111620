ddd510636affc2cc04eacbe7323b58bbf50bb676
Alex Marz <alexhmarz@gmail.com>
2020-11-05 18:35:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,11 +5,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inside_key, name|
-      binding.pry
       name.each do |
-    final_array
+    end
   end
-end
 
   
     
