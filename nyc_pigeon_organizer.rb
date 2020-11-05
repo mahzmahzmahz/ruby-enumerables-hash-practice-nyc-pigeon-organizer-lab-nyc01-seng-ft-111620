b6c44510955861878cb3a7299a4a16cca701fcf4
Alex Marz <alexhmarz@gmail.com>
2020-11-05 18:49:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
+  
+  sorted_list = data.each_with_object({}) do |(key, value), final_array|
+    #binding.pry
+    value.each do |inside_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {} 
+        end
+        if !final_array[name][key] 
+          !final_array[name][key] = []
+        end
+      final_array[name][key].push(inside_key.to_s)
+      end
+    end
+  end
+  #binding.pry
+ end
+
+  
+    
+    
+
+  
   # write your code here!
-end
+  #binding.pry
+
