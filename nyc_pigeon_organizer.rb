0cc3d229565c7b8f09f81e310a18ba8a0f8ca8a2
Alex Marz <alexhmarz@gmail.com>
2020-11-05 18:43:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
-  data.each_with_object({}) do |(key, value), final_array|
+  sorted_list = data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
     value.each do |inside_key, names|
       names.each do |name|
